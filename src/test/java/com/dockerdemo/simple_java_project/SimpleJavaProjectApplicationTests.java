8097357d5887bca3791febaa25ae7ffc93590818
vikas puri <vikaspuri422@gmail.com>
2025-08-19 13:57:46 +0530

    First Commit

---
+++ src/test/java/com/dockerdemo/simple_java_project/SimpleJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerdemo.simple_java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
